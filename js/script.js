a98914e0fb6dd8f284b7c8db29b9898c45bf83b2
zenzlia <42208300+zenzlia@users.noreply.github.com>
2019-02-05 23:42:17 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+
+    var bannedStages = [];
+    var player1 = "active";
+    var player2 = "inactive";
+    var bans = 3;
+    var isGame1 = false;
+
+    var gameSwitch = document.getElementById("gameSwitch");
+
+    
+
+    $("#game1Switch").click(function(event) {
+        isGame1=true;
+        bans = 4;
+        gameSwitch.remove();
+        document.getElementById("choosingPlayer").innerHTML = "Winner is Choosing";
+        document.getElementById("counterpicks").remove();
+        $("#starters").children().children().css({"height": "350px","width":"600px","margin":"5px"});
+    });
+
+    $("img").click(function(event) {
+        if(gameSwitch) {
+            gameSwitch.remove();
+        }
+        var imageClicked = $(event.target);
+    
+        if(isGame1) {
+            game1(imageClicked);
+        } else {
+            game2Plus(imageClicked);
+        
+        }
+        
+        
+    });
+        
+    function game1(stage){
+        if(bans == 4) {
+            document.getElementById("choosingPlayer").innerHTML = "Winner is Choosing";
+            bannedStages.push(stage);
+            stage.addClass(findActiveClass);
+            switchPlayer();
+        } else if (bans > 1){
+            stage.addClass(findActiveClass);
+            bannedStages.push(stage);
+        } else {
+            stage.addClass("chosen");
+            var stageName = stage.attr("alt");
+            document.getElementById("choosingPlayer").innerHTML = "To " + stageName + " we go!";
+        }
+
+        if(bans == 2){
+            document.getElementById("choosingPlayer").innerHTML = "Winner Choose a Stage";
+        }
+
+        bans--;
+        console.log(bans);
+    }   
+
+    function game2Plus(stage) {
+        if(bans == 3) {
+            stage.addClass(findActiveClass);
+            bannedStages.push(stage);
+            switchPlayer();
+        } else if(bans == 2 ) {
+            stage.addClass(findActiveClass);
+            bannedStages.push(stage);
+            document.getElementById("choosingPlayer").innerHTML = "Loser Choose a Stage";
+        } else {
+            stage.addClass("chosen");
+            var stageName = stage.attr("alt");
+            document.getElementById("choosingPlayer").innerHTML = "To " + stageName + " we go!";
+        }
+        bans--;
+        console.log(bans);
+
+    }
+
+    //Checks if the image has already been selected
+    function isSelected(image) {
+        if(image.hasClass("player1banned") || image.hasClass("player2banned")) {
+            return true;
+        } else{
+            return false
+        }
+    }
+
+    //Switches the Player Banning, Probably not needed at the moment
+    function switchPlayer() {
+        if(player1 == "active"){
+            player2 = "active";
+            player1 = "inactive";
+            document.getElementById("choosingPlayer").innerHTML = "Loser is Choosing";
+        } else {
+            player1 = "active"
+            player2 = "inactive"
+            document.getElementById("choosingPlayer").innerHTML = "Winner is Choosing";
+        }
+    }
+
+    function isActive(player) {
+        if(player == "active") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function findActiveClass() {
+        if(isActive(player1)) {
+            return "player1banned";
+        } else {
+            return "player2banned";
+        }
+    }
+
+});
